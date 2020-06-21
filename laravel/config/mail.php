57a0405484bf2e3083b3076755bedbaba2681826
ylafaro0310 <y16mukurou@gmail.com>
2020-06-21 12:01:40 +0900

    Modify config/mail.php

--- laravel/config/mail.php
+++ laravel/config/mail.php
@@ -87,6 +87,10 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
+    'to' => [
+        'address' => env('MAIL_TO_ADDRESS'),
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
